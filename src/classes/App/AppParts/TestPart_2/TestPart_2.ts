1190869cb295dacfa54b37f0d2c97f2469369974
Danila-software-engineer <69984315+Danila-software-engineer@users.noreply.github.com>
2022-10-18 14:25:31 +0300

    feat(test part): add TestPart_2

---
+++ src/classes/App/AppParts/TestPart_2/TestPart_2.ts
@@ -0,0 +1,34 @@
+import {IUniqueParts, REGISTRY_PART} from "@classes/App/AppTID";
+import {IPart3D} from "@classes/Part/PartTID";
+
+const NAME = 'TestPart_2';
+declare module '@classes/App/AppTID' {
+    export interface IUniqueParts {
+        [NAME]:TestPart_2
+    }
+}
+
+@REGISTRY_PART(NAME)
+export class TestPart_2 implements IPart3D{
+    disable(): void {
+        console.log('DISABLE_TEST_PT_2');
+    }
+
+    dispose(): void {
+        console.log('DISPOSE_TEST_PT_2');
+    }
+
+    enable(): void {
+        console.log('ENABLE_TEST_PT_2');
+    }
+
+    init(...arg: any): Promise<void> {
+        console.log('INIT_TEST_PT_2');
+        return Promise.resolve(undefined);
+    }
+
+    get partUID(): keyof IUniqueParts {
+        return NAME;
+    }
+
+}
