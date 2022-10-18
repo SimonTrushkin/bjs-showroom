bc903aec65dd1c93a7d843fd40e46735bb33f40c
SimonTrushkin <1blenderman2@gmail.com>
2022-10-18 15:40:34 +0300

    feat(Registry): Add registry file for parts.

---
+++ src/classes/App/AppParts/PART_REGISTRY.ts
@@ -0,0 +1,2 @@
+import "@classes/App/AppParts/TestPart_1/TestPart_1";
+import "@classes/App/AppParts/TestPart_2/TestPart_2";
--- src/routes/test-space/index.ts
+++ src/routes/test-space/index.ts
@@ -1,4 +1,5 @@
 import {App} from "@classes/App/App";
+import '@classes/App/AppParts/PART_REGISTRY';
 
 //@ts-ignore
 window.App = App;
